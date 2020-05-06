7cb1aeb8b01f81fde09503411ef0b75a2b4365c5
Jacob Gold <jkobygold@gmail.com>
2020-05-05 22:48:19 -0400

    Updates

---
+++ precache-manifest.d9e82349dc758136147b944b6e7e59ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f6819bb69d78a324d841e4518147d7",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "38f0f2b173dbccc138ac",
+    "url": "/clicky-game/static/css/2.16c59a73.chunk.css"
+  },
+  {
+    "revision": "db1ec14ca3d6648b21f3",
+    "url": "/clicky-game/static/css/main.66af7960.chunk.css"
+  },
+  {
+    "revision": "38f0f2b173dbccc138ac",
+    "url": "/clicky-game/static/js/2.568784bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.568784bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1ec14ca3d6648b21f3",
+    "url": "/clicky-game/static/js/main.813ad2a9.chunk.js"
+  },
+  {
+    "revision": "b631d7080dcb249ab8e5",
+    "url": "/clicky-game/static/js/runtime-main.ea51dac7.js"
+  }
+]);
